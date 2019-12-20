12ea5ce396636eecd8060abde000f394282c30dc
Wesley Matos <wesley.matos@redfox.tech>
2019-12-20 13:40:48 -0300

    save users in redis

--- src/modules/users/repository.js
+++ src/modules/users/repository.js
@@ -1,3 +1,15 @@
+const { client } = require('../../redis');
+const { REDIS_EXPIRES } = require('../../config');
+
+/**
+ * Save users in redis.
+ * @param {Object} data Users to save.
+ * @param {Number} time Duration of register in redis db.
+ */
+const saveUsers = (data, time = REDIS_EXPIRES) => {
+  client.set('users', JSON.stringify(data), 'EX', time);
+};
 
 module.exports = {
+  saveUsers,
 };
