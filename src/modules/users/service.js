ca62df48d65f85cd986543faddc91ee04d79b45d
Wesley Matos <wesley.matos@redfox.tech>
2019-12-20 13:45:06 -0300

    write method to get users

--- src/modules/users/service.js
+++ src/modules/users/service.js
@@ -1,2 +1,25 @@
+const { getUsers: getUsersIntegration } = require('./integration');
+const { findUsers: usersFromDb, saveUsers } = require('./repository');
+
+/**
+ * Get users from db or integration.
+ */
+const getUsers = async () => {
+  try {
+    const usersFromRedis = await usersFromDb();
+
+    if (usersFromRedis) return usersFromRedis;
+
+    const usersFromIntegration = await getUsersIntegration();
+
+    saveUsers(usersFromIntegration);
+
+    return usersFromIntegration;
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
+  getUsers,
 };
