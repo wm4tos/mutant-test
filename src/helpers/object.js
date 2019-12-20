77033225ba5c71e9670636797f9274cdd3ac14a2
Wesley Matos <wesley.matos@redfox.tech>
2019-12-20 16:46:12 -0300

    write function to get deep entries of object

--- src/helpers/object.js
+++ src/helpers/object.js
@@ -4,6 +4,9 @@ const getValue = (obj, key) => {
   return getValue(obj[keys.shift()], keys.join('.'));
 };
 
+const getDeepEntries = (obj, prefix = '') => Object.entries(obj).flatMap(([k, v]) => (Object(v) === v ? getDeepEntries(v, `${prefix}${k}.`) : [[`${prefix}${k}`, v]]));
+
 module.exports = {
   getValue,
+  getDeepEntries,
 };
