7ab03706855426510d086c5668047c3a765a7e0e
Wesley Matos <wesley.matos@redfox.tech>
2019-12-20 15:22:54 -0300

    write routes to get all users

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -1,3 +1,21 @@
+const { getUsers: getUsersService } = require('./service');
+const { httpCodes } = require('../../helpers');
+
+const getAllUsers = async (req, res) => {
+  try {
+    const data = await getUsersService();
+
+    const response = {
+      ...httpCodes(),
+      data,
+    };
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
+  getAllUsers,
 };
--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -1,7 +1,17 @@
 const { Router } = require('express');
 
+const { getAllUsers } = require('./controller');
+
 module.exports = () => {
   const router = Router();
+
+  /**
+   * Get all users
+   * @route GET /user
+   * @group User - Operations about users.
+   * @returns {User[]} 200 - An array with user data
+   */
+  router.get('/', getAllUsers);
 
   return { router, endpoint: '/user' };
 };
