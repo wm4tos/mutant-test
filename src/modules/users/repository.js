eb9d2ce6e6188b8121ccdc56448820d480a60e3f
Wesley Matos <wesley.matos@redfox.tech>
2019-12-20 13:41:00 -0300

    get users from redis

--- src/modules/users/repository.js
+++ src/modules/users/repository.js
@@ -2,6 +2,21 @@ const { client } = require('../../redis');
 const { REDIS_EXPIRES } = require('../../config');
 
 /**
+ * Get users from redis.
+ */
+const findUsers = async () => {
+  try {
+    const data = await client.getAsync('users');
+
+    if (data) return JSON.parse(data);
+
+    return null;
+  } catch (error) {
+    throw error;
+  }
+};
+
+/**
  * Save users in redis.
  * @param {Object} data Users to save.
  * @param {Number} time Duration of register in redis db.
@@ -11,5 +26,6 @@ const saveUsers = (data, time = REDIS_EXPIRES) => {
 };
 
 module.exports = {
+  findUsers,
   saveUsers,
 };
