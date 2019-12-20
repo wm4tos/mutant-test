7f156a04e346d5792b547ff900e98d11b2621c46
Wesley Matos <wesley.matos@redfox.tech>
2019-12-20 14:00:26 -0300

    treat axios response inside integration file

--- src/modules/users/integration.js
+++ src/modules/users/integration.js
@@ -3,7 +3,15 @@ const axios = require('./axios');
 /**
  * Get users from external API.
  */
-const getUsers = async () => axios.get('/users');
+const getUsers = async () => {
+  try {
+    const { data } = await axios.get('/users');
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
   getUsers,
