9664c81a3a0d727e466cd460b96450deb2bb07ed
Wesley Matos <wesley.matos@redfox.tech>
2019-12-20 11:39:54 -0300

    remove routes

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -1,23 +1,3 @@
-const { signIn, create: createUser } = require('./service');
 
-module.exports.auth = async (req, res, next) => {
-  try {
-    const { body } = req;
-    const data = await signIn(body);
-
-    res.status(200).json(data);
-  } catch (error) {
-    next(error);
-  }
-};
-
-module.exports.create = async (req, res, next) => {
-  try {
-    const { body } = req;
-    const data = await createUser(body);
-
-    res.status(200).json(data);
-  } catch (error) {
-    next(error);
-  }
+module.exports = {
 };
--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -1,48 +1,7 @@
 const { Router } = require('express');
-const { auth: authRoute, create } = require('./controller');
-const contracts = require('./contract');
-const validator = require('../../middlewares/validator');
-const { auth, checkPermission } = require('../../middlewares');
 
 module.exports = () => {
   const router = Router();
-
-  /**
-   * @typedef Login
-   * @property {string} email.required
-   * @property {string} password.required
-   */
-
-  /**
-   * @route POST /user/auth
-   * @group User - Operations about user
-   * @param {Login.model} user.body - E-mail and password of user
-   * @returns {object} 200 - User authenticated successfully
-   * @returns {object} 400 - Body of request malformed
-   * @returns {object} 401 - User or password incorrect
-   */
-  router.post('/auth', validator(contracts.auth, 'body'), authRoute);
-
-  /**
-   * @typedef User
-   * @property {string} name.required
-   * @property {string} email.required
-   * @property {string} phone.required
-   * @property {string} password.required
-   * @property {boolean} admin
-   */
-
-  /**
-   * @route POST /user/create
-   * @group User - Operations about user
-   * @param {User.model} user.body - Data of user
-   * @returns {object} 200 - User created successfully
-   * @returns {object} 400 - Body of request malformed
-   * @returns {object} 401 - No token provided
-   * @returns {object} 409 - Duplicate user
-   * @security JWT
-   */
-  router.post('/create', auth, validator(contracts.create, 'body'), checkPermission, create);
 
   return { router, endpoint: '/user' };
 };
